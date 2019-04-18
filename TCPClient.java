acbb58bc0e96d016a9632ca13a475bc9c2979a6b
marwan98youssef <49454023+marwan98youssef@users.noreply.github.com>
2019-04-18 22:18:24 +0200

    Add files via upload

--- TCPClient.java
+++ TCPClient.java
@@ -1,35 +1,64 @@
 package project;
-import java.io.*; 
+
+import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class TCPClient {
+	public static Socket socket;
+	public static BufferedReader reader;
+	public static PrintWriter writer;
 
-	    public static void main(String argv[]) throws Exception 
-	    { 
-	        String sentence; 
-	        String modifiedSentence; 
+	public static void startListening() {
+		Thread listeningThread = new Thread(new Runnable() {
 
-	        BufferedReader inFromUser = 
-	          new BufferedReader(new InputStreamReader(System.in)); 
+			@Override
+			public void run() {
+				String line = null;
+				try {
+					while ((line = reader.readLine()) != null) {
+						System.out.println("Server: " + line);
+					}
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		});
+		listeningThread.start();
+	}
 
-	        Socket clientSocket = new Socket("localhost", 6789); 
+	public static void startConnection() {
+		try {
 
-	        DataOutputStream outToServer = 
-	          new DataOutputStream(clientSocket.getOutputStream()); 
-	        
-	        BufferedReader inFromServer = 
-	                new BufferedReader(new
-	                InputStreamReader(clientSocket.getInputStream())); 
+			socket = new Socket("192.168.43.165", 6789);
+			System.out.println("Connection started");
+			reader = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			writer = new PrintWriter(new OutputStreamWriter(
+					socket.getOutputStream()));
+			startListening();
+			Scanner sc = new Scanner(System.in);
+			String line = null;
+			while ((line = sc.nextLine()) != null) {
+				writer.println(line);
+				writer.flush();
+			}
 
-	              sentence = inFromUser.readLine(); 
+		} catch (Exception e) {
+			System.out.println("Connection closed");
+		}
+	}
 
-	              outToServer.writeBytes(sentence + '\n'); 
+	public static void main(String argv[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String command = sc.nextLine();
+		while (true) {
+			if (command.equals("CONNECT")) {
+				startConnection();
+				break;
+			}
+			command = sc.nextLine();
+		}
+	}
 
-	              modifiedSentence = inFromServer.readLine(); 
-
-	              System.out.println("FROM SERVER: " + modifiedSentence); 
-
-	              clientSocket.close(); 
-	                         
-	          } 
-	      } 
+}
--- TCPServer.java
+++ TCPServer.java
@@ -1,32 +1,70 @@
 package project;
 
-import java.io.*; 
+import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+
 public class TCPServer {
+	public static Socket socket;
+	public static PrintWriter writer;
+	public static BufferedReader reader;
 
-	  public static void main(String argv[]) throws Exception 
-	    { 
-	      String clientSentence; 
-	      String capitalizedSentence; 
+	public static void startListening() {
+		Thread listeningThread = new Thread(new Runnable() {
 
-	      ServerSocket welcomeSocket = new ServerSocket(6789); 
-	  
-	      while(true) { 
-	            Socket connectionSocket = welcomeSocket.accept(); 
+			@Override
+			public void run() {
+				String line = null;
+				try {
+					while ((line = reader.readLine()) != null) {
+						System.out.println("Client: " + line);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		listeningThread.start();
+	}
 
-	           BufferedReader inFromClient = 
-	              new BufferedReader(new
-	              InputStreamReader(connectionSocket.getInputStream()));
+	public static void startConnection() {
+		new Thread(new Runnable() {
 
+			@Override
+			public void run() {
+				try {
+					ServerSocket server = new ServerSocket(6789);
+					System.out.println("Wating for client");
+					while (true) {
+						try {
+							socket = server.accept();
+							System.out.println("Connection built");
+							reader = new BufferedReader(new InputStreamReader(
+									socket.getInputStream()));
+							writer = new PrintWriter(new OutputStreamWriter(
+									socket.getOutputStream()));
+							startListening();
+						} catch (Exception e) {
 
-	           DataOutputStream  outToClient = 
-	             new DataOutputStream(connectionSocket.getOutputStream()); 
+						}
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-	           clientSentence = inFromClient.readLine(); 
+			}
+		}).start();
 
-	           capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
+	}
 
-	           outToClient.writeBytes(capitalizedSentence); 
-	        } 
-	    } 
-	} 
+	public static void main(String argv[]) throws Exception {
+		startConnection();
+		Scanner sc = new Scanner(System.in);
+		String line = null;
+		while ((line = sc.nextLine()) != null) {
+			writer.println(line);
+			writer.flush();
+		}
+	}
+}
